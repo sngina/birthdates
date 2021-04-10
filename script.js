44104ffdc26c5a088ffe06d869d891ff52ab9847
sngina <sngina707@gmail.com>
2021-04-10 22:25:51 +0300

    add forms in html

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 //name of the males
-
+function mighty () {
 var maleNames = ["Kwasi" , "Kwadwo" ,"Kwabena" ,"Kwaku" ,"Yaw" ,"Kofi" ,"Kwame"] ;
+var femaleNames = ["Akosua" ,"Adowoa" ,"Abenaa" ,"Akua" , "Yaa" ,"Afua" ,"Ama"]; 
 var weekDay =["Sunday" , "Monday" ,"Tuesday" ,"Wednesday" ,"Thursday" ,"Friday", "Saturday"];
- console.log(maleNames);
-//name of the males in a day
-// for ((weekDay) = ( ( (2000/4) -2000*1489-1) + ((5*2001/4) ) + ((26*(10+1)/10)) + Monday ) % 7;
 
-//name the females
 
-var femaleNames = ["Akosua" ,"Adowoa" ,"Abenaa" ,"Akua" , "Yaa" ,"Afua" ,"Ama"];
-var weekDay =["Sunday" ,"Monday" ,"Tuesday" ,"Wednesday" ,"Thursday" ,"Friday" ,"Saturday"];
-   
+}
+ 
