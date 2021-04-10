6d6fd3be15be25fd908ebba2a0de646fbd8fb955
sngina <sngina707@gmail.com>
2021-04-10 11:36:16 +0300

    add variables in script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+//name of the males
+
+var maleNames = ["Kwasi" , "Kwadwo" ,"Kwabena" ,"Kwaku" ,"Yaw" ,"Kofi" ,"Kwame"] ;
+var weekDay =["Sunday" , "Monday" ,"Tuesday" ,"Wednesday" ,"Thursday" ,"Friday", "Saturday"];
+ console.log(maleNames);
+//name of the males in a day
+// for ((weekDay) = ( ( (2000/4) -2000*1489-1) + ((5*2001/4) ) + ((26*(10+1)/10)) + Monday ) % 7;
+
+//name the females
+
+var femaleNames = ["Akosua" ,"Adowoa" ,"Abenaa" ,"Akua" , "Yaa" ,"Afua" ,"Ama"];
+var weekDay =["Sunday" ,"Monday" ,"Tuesday" ,"Wednesday" ,"Thursday" ,"Friday" ,"Saturday"];
+   
