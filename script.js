9b83b7ec56fe6e81924a5d4180d9fbac8224c310
sngina <sngina707@gmail.com>
2021-04-11 12:24:48 +0300

    change variable name

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //name of the males and females plus the days of the week
 function mighty () {
-var maleNames = ["Kwasi" , "Kwadwo" ,"Kwabena" ,"Kwaku" ,"Yaw" ,"Kofi" ,"Kwame"] ;
-var femaleNames = ["Akosua" ,"Adowoa" ,"Abenaa" ,"Akua" , "Yaa" ,"Afua" ,"Ama"]; 
+var maleName = ["Kwasi" , "Kwadwo" ,"Kwabena" ,"Kwaku" ,"Yaw" ,"Kofi" ,"Kwame"] ;
+var femaleName = ["Akosua" ,"Adowoa" ,"Abenaa" ,"Akua" , "Yaa" ,"Afua" ,"Ama"]; 
 var weekDay =["Sunday" , "Monday" ,"Tuesday" ,"Wednesday" ,"Thursday" ,"Friday", "Saturday"];
 
 
@@ -17,46 +17,46 @@ var date = new Date(combinedDate);
     let dayBorn = date.getDay();
   if(gender=="Male"){
       if(dayBorn==0){
-          alert(+" You were born on "+weekDays[0]+" and your Akan name is "+ maleNames[0])
+          alert(+" You were born on "+weekDays[0]+" and your Akan name is "+ maleName[0])
       }
       if(dayBorn==1){
-        alert("You were born on "+weekDay[1]+" and your Akan name is "+ maleNames[1])
+        alert("You were born on "+weekDay[1]+" and your Akan name is "+ maleName[1])
     }
     if(dayBorn==2){
-        alert("You were born on "+weekDay[0]+" and your Akan name is "+ maleNames[2])
+        alert("You were born on "+weekDay[0]+" and your Akan name is "+ maleName[2])
     }
     if(dayBorn==3){
-        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[3])
+        alert("You were born on "+weekDay[0]+" and your Akan name is "+ maleName[3])
     }
     if(dayBorn==4){
-        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[4])
+        alert("You were born on "+weekDay[0]+" and your Akan name is "+ maleName[4])
     }
     if(dayBorn==5){
-        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[5])
+        alert("You were born on "+weekDay[0]+" and your Akan name is "+ maleName[5])
     }
     
     if(dayBorn==6){
-        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[6])
+        alert("You were born on "+weekDay[0]+" and your Akan name is "+ maleName[6])
     }
   }
   if(gender=="Female"){
     if(dayBorn==0){
-        alert("You were born on "+days[0]+" and your Akan name is "+akanFemale[0])
+        alert("You were born on "+weekDay[0]+" and your Akan name is "+femaleName[0])
     }
     if(dayBorn==1){
-      alert("You were born on "+days[1]+" and your Akan name is "+akanFemale[1])
+      alert("You were born on "+weekDay[1]+" and your Akan name is "+akanFemale[1])
   }
   if(dayBorn==2){
-      alert("You were born on "+days[2]+" and your Akan name is "+akanFemale[2])
+      alert("You were born on "+weekDay[2]+" and your Akan name is "+akanFemale[2])
   }
   if(dayBorn==3){
-      alert("You were born on "+days[3]+" and your Akan name is "+akanFemale[3])
+      alert("You were born on "+weekDay[3]+" and your Akan name is "+akanFemale[3])
   }
   if(dayBorn==4){
-      alert("You were born on "+days[4]+" and your Akan name is "+akanFale[4])
+      alert("You were born on "+weekDay[4]+" and your Akan name is "+akanFale[4])
   }
   if(dayBorn==5){
-      alert("You were born on "+days[5]+" and your Akan name is "+akanFemale[5])
+      alert("You were born on "+weekDay[5]+" and your Akan name is "+akanFemale[5])
   }
   
   if(dayBorn==6){
